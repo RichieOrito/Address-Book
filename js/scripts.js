b6c13e4f1239fb8846ed9fdfcf5013196c11d80e
RichieOrito <richard.omondi@student.moringaschool.com>
2021-11-25 09:08:34 +0300

    Adds the business logic and user interface logic for the Addresses

--- js/scripts.js
+++ js/scripts.js
@@ -14,22 +14,50 @@ Contact.prototype.fullName = function(){
   return this.firstName + " " + this.lastName;
 }
 
+Address.prototype.fullAddress = function() {
+  return this.street + " " + this.city + " " + this.county;
+}
+
 //user interface logic
 $(document).ready(function(){
+
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-county">County</label>' +
+                                   '<input type="text" class="form-control new-county">' +
+                                 '</div>' +
+                               '</div>');
+  });
+
   $("form#new-contact").submit(function(event){
     event.preventDefault();
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-
     var newContact = new Contact(inputtedFirstName, inputtedLastName);// creates a new contact object
     //  by calling the contact construct and passing it the inputted data
     //  the variable newContact now refers to that newly created contact object which knows its own
     //  firstName and lastName
-    
+
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedCounty = $(this).find("input.new-county").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedCounty);
+      newContact.addresses.push(newAddress);
+    });
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-    $("input#new-first-name").val("")
+    $("input#new-first-name").val("");
     $("input#new-last-name").val("");
 
     $(".contact").last().click(function() {
@@ -37,11 +65,23 @@ $(document).ready(function(){
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address){
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+      });
     });
-  
-  });
 
- 
+    function resetFields() {
+       $("input#new-first-name").val("");
+       $("input#new-last-name").val("");
+       $("input.new-street").val("");
+       $("input.new-city").val("");
+       $("input.new-county").val("");
+   }
+
+  });
+    
+    
 
 });
 
